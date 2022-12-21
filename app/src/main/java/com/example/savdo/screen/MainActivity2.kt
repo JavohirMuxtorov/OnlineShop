90e6d28eacf486a1c4aab0b09d2fab62a5252c48
Javohirbek <muxtorovj74@gmail.com>
2022-12-21 13:57:10 +0500

    commit first

---
+++ app/src/main/java/com/example/savdo/screen/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.savdo.screen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.savdo.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
